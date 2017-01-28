a89159d21444dd46bcbba7cb9b2316130009f4be
AraHaan <seandhunt_7@yahoo.com>
2017-01-28 17:52:32 -0600

    Python 3.6.0 for Cygwin.
    This is a WIP for Python 3.6.0 under Cygwin.
    Note: This does contain some patches from after the official 3.6.0
    release that will be in 3.6.1 and 3.7.0.

---
+++ Python/getcompiler.c
@@ -0,0 +1,32 @@
+
+/* Return the compiler identification, if possible. */
+
+#include "Python.h"
+
+#ifndef COMPILER
+
+#ifdef __GNUC__
+#define COMPILER "\n[GCC " __VERSION__ "]"
+#endif
+
+#ifdef __CLANG__
+#define COMPILER "\n[CLANG " __VERSION__ "]"
+#endif
+
+#endif /* !COMPILER */
+
+#ifndef COMPILER
+
+#ifdef __cplusplus
+#define COMPILER "[C++]"
+#else
+#define COMPILER "[C]"
+#endif
+
+#endif /* !COMPILER */
+
+const char *
+Py_GetCompiler(void)
+{
+	return COMPILER;
+}
